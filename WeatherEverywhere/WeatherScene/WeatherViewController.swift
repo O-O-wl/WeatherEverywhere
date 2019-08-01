88a49a26e93e7410057215ce15ff1d6dc5028bde
부엉이 <o.o.wl.soft@gmail.com>
2019-08-02 03:11:51 +0900

    feat: Start

---
+++ WeatherEverywhere/WeatherScene/WeatherViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherEverywhere
+//
+//  Created by 이동영 on 01/08/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class WeatherViewController: UIViewController {
+    
+    // MARK: - Properties
+   
+    // MARK: - IBOutlet
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - IBActions
+    
+    
+}
