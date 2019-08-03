6b96010b0602662b35f4b113b293f266f8413cd7
부엉이 <o.o.wl.soft@gmail.com>
2019-08-04 02:31:13 +0900

    refactor: Modify DispatchQueue.global to DispatchQueue.main

--- WeatherEverywhere/API/WeatherAPI.swift
+++ WeatherEverywhere/API/WeatherAPI.swift
@@ -7,7 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
-class WeatherAPI: NSObject {
-
+struct WeatherAPI {
+    static private let baseURL =  URL(string: "https://api.darksky.net/forecast")
+    static private let apiKey = { Bundle.main.infoDictionary?["APIKey"] as? String ?? "" }()
+    
+    static func requestForecast(query: Queriable, completion: @escaping (ForcastDTO) -> Void ) {
+        let reqURL = baseURL?.appendingPathComponent(apiKey).appendingPathComponent(query.toQuery())
+        
+        DispatchQueue.main.async {
+            guard let data = try? Data(contentsOf: reqURL!) else { return }
+            print(String(data: data,encoding: .utf8)!)
+            let forcast = try! JSONDecoder().decode(ForcastDTO.self, from: data)
+            completion(forcast)
+        }
+    }
 }
