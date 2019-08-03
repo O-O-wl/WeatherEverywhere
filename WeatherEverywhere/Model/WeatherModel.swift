59d5888ceabbe9fdc1e4161b3e15a66944874cc0
부엉이 <o.o.wl.soft@gmail.com>
2019-08-03 17:41:27 +0900

    feat: Add UV

---
+++ WeatherEverywhere/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  WeatherEverywhere
+//
+//  Created by 이동영 on 03/08/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherEverywhere/Utill/UV.swift
@@ -0,0 +1,17 @@
+//
+//  UV.swift
+//  WeatherEverywhere
+//
+//  Created by 이동영 on 03/08/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import Foundation
+
+struct UV: Textable {
+    private let level: Int
+    
+    var description: String {
+        return String(level)
+    }
+}
