694cf2994ba4118a69e3fedea9f9f70fc9421fa5
부엉이 <o.o.wl.soft@gmail.com>
2019-08-04 23:09:05 +0900

    feat: Implement Parser

--- WeatherEverywhere/Model/Parser.swift
+++ WeatherEverywhere/Model/Parser.swift
@@ -9,13 +9,55 @@
 import Foundation
 
 struct Parser {
-    static func parse(dto: ForcastDTO) -> ForcastModelable? {
-        return nil
+    static func parse(forcastDTO: ForcastDTO, title: String) -> ForcastModelable? {
+        let forcastModel = WeatherModel2(
+            location: LocationModel(
+                title: title,
+                latitude: forcastDTO.latitude,
+                longitude: forcastDTO.longitude),
+            daily: forcastDTO.daily.data.map { parse(dailyDTO: $0)},
+            hourly: forcastDTO.daily.data.map { parse(hourlyDTO: $0)}
+        )
+        return forcastModel
     }
-    static func parse(_ daily: WeatherDTO ) -> DailyWeatherModelable? {
-        return nil
+    
+    static func parse(dailyDTO: WeatherDTO) -> DailyWeatherModelable {
+        let dailyModel = WeatherModel2(
+            icon: dailyDTO.icon,
+            day: Day(dailyDTO.time),
+            temperatureMax: Temperature.init(dailyDTO.temperatureMax ?? 0),
+            temperatureMin: Temperature(dailyDTO.temperatureMin ?? 0)
+        )
+        
+        return dailyModel
     }
-    static func parse(_ hourly: WeatherDTO ) -> HourlyWeatherModelable? {
-        return nil
+    
+    static func parse(hourlyDTO: WeatherDTO) -> HourlyWeatherModelable {
+        let hourlyModel = WeatherModel2(
+            icon: hourlyDTO.icon,
+            time: Time(hourlyDTO.time),
+            temperature: Temperature(hourlyDTO.temperature ?? 0)
+        )
+        return hourlyModel
+    }
+    
+    static func parse(currentDTO: WeatherDTO) -> CurrentWeatherModelable {
+        let currentModel = WeatherModel2(
+            summary: currentDTO.summary,
+            sunriseTime: Time.init(currentDTO.sunriseTime ?? 0),
+            sunsetTime: Time.init(currentDTO.sunsetTime ?? 0),
+            precipIntensity: Precipitation(currentDTO.precipIntensity),
+            precipProbability: Persentage(currentDTO.precipProbability),
+            humidity: Persentage(currentDTO.humidity),
+            pressure: Pressure(currentDTO.pressure),
+            wind: Wind(speed: currentDTO.windSpeed, bearing: currentDTO.windBearing),
+            uv: UV(currentDTO.uvIndex),
+            visibility: Visibility(currentDTO.visibility),
+            temperature: Temperature(currentDTO.temperature ?? 0),
+            apparentTemperature: Temperature(currentDTO.apparentTemperature ?? 0),
+            temperatureMax: Temperature(currentDTO.temperatureMax ?? 0),
+            temperatureMin: Temperature(currentDTO.temperatureMin ?? 0)
+            )
+        return currentModel
     }
 }
