678e8e2c00fb8b33e65620decec2ab124bfee880
부엉이 <o.o.wl.soft@gmail.com>
2019-08-05 00:14:32 +0900

    feat: Define Wallpaper;

--- WeatherEverywhere/Util/Wallpaper.swift
+++ WeatherEverywhere/Util/Wallpaper.swift
@@ -5,5 +5,19 @@
 //  Created by 이동영 on 05/08/2019.
 //  Copyright © 2019 부엉이. All rights reserved.
 //
-
+import UIKit
 import Foundation
+
+struct Wallpaper: Displayable {
+    private static let prefix: String = "Wallpaper"
+    private let resourceName: String
+    
+    var description: UIImage? {
+        let imageName = Wallpaper.prefix + resourceName
+        return UIImage(named: imageName)
+    }
+    
+    init(_ resourceName: String) {
+        self.resourceName = resourceName
+    }
+}
