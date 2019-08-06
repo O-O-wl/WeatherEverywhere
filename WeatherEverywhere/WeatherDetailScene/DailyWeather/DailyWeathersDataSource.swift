214a4de53e9d1f90c0f9793ca33920943322a707
부엉이 <o.o.wl.soft@gmail.com>
2019-08-06 17:47:27 +0900

    save: save point before AutoLayout

---
+++ WeatherEverywhere/WeatherDetailScene/DailyWeather/DailyWeathersDataSource.swift
@@ -0,0 +1,41 @@
+//
+//  DailyWeatherTableView.swift
+//  WeatherEverywhere
+//
+//  Created by 이동영 on 05/08/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import UIKit
+
+class DailyWeathersDataSource: NSObject, UITableViewDataSource {
+    
+    var dailyWeathers: [DailyWeatherModelable]
+    
+    init(dailyWeathers models: [DailyWeatherModelable]) {
+        self.dailyWeathers = models
+    }
+    
+}
+// MARK: - DataSource
+extension DailyWeathersDataSource:  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dailyWeathers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dailyWeatherCell", for: indexPath)
+        
+        guard let dailyCell = cell as? DailyWeatherCell else {
+            return cell
+        }
+        dailyCell.sync(model: dailyWeathers[indexPath.row])
+        return dailyCell
+    }
+    
+    
+}
+// MARK: - Delegate
+extension DailyWeathersDataSource: UITableViewDelegate {
+    
+}
